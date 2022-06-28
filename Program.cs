511c8af3d6ad8088279e03491d71735c004dfdc6
Usama Fazal <Usama Fazal@WIN-VOL3D49H6UK>
2022-06-29 00:44:49 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace QuestionNo7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myList = {1,2,3,4,5,6,7,8,9,10,11, 12};
+
+            Console.WriteLine("Elements in odd positions in myList are: ");
+            // by using i=i+2, we ensure the elements selected are 1st, 3rd, 5th, and so on...
+
+            for (int i = 0; i < myList.Length; i = i + 2)
+            { 
+                Console.WriteLine(myList[i]);
+            }
+        }
+    }
+}
